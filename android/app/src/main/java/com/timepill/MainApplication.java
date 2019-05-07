c95496b6a40f22bea4f6c51d8203370f5ad26649
li.zhaoguang <li.zhaoguang@immomo.com>
2019-05-07 17:17:12 +0800

    react-native-navigation android 编译通过

--- android/app/src/main/java/com/timepill/MainApplication.java
+++ android/app/src/main/java/com/timepill/MainApplication.java
@@ -11,7 +11,43 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
+
+public class MainApplication extends NavigationApplication {
+
+  @Override
+  protected ReactGateway createReactGateway() {
+    ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
+    };
+    return new ReactGateway(this, isDebug(), host);
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+            // eg. new VectorIconsPackage()
+    );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
