beb5c0ec2144b17b7c1ad525777cbe4021589ac9
xuwenyang <xuwenyang@teddymobile.cn>
2019-05-18 15:06:41 +0800

    1. 去掉customedList组件，避免增加代码复杂度

--- src/component/follow/followUserList.js
+++ src/component/follow/followUserList.js
@@ -6,7 +6,6 @@ import Touchable from '../touchable';
 import Color from '../../style/color';
 
 import UserIcon from '../userIcon';
-import CustomedList from '../customedList';
 
 
 export default class FollowUserList extends Component {
@@ -15,33 +14,102 @@ export default class FollowUserList extends Component {
         super(props);
 
         this.dataSource = props.dataSource;
-        this.listType = props.listType;
+        this.listType = props.listType || 'undefined';
+
+        this.state = {
+            users: [],
+            hasMore: false,
+
+            refreshing: false,
+            refreshFailed: false
+        };
+    }
+
+    componentDidMount() {
+        InteractionManager.runAfterInteractions(() => {
+            this.refresh();
+        });
+    }
+
+    refresh(loadMore = false) {
+        if (this.state.refreshing) {
+            return;
+        }
+
+        this.setState({hasMore: false, refreshing: true, refreshFailed: false});
+        this.dataSource.refresh(loadMore)
+                .then(result => {
+                    if(!result) {
+                        throw {
+                            message: 'refresh ' + this.listType + ' no result'
+                        }
+
+                    } else {
+                        console.log('refresh ' + this.listType + ' result:', result);
+
+                        this.setState({
+                            users: result.list ? result.list : [],
+                            hasMore: result.more,
+                            refreshFailed: false
+                        });
+                    }
+
+                }).catch(e => {
+                    this.setState({
+                        users: [],
+                        hasMore: false,
+                        refreshFailed: true
+                    });
+
+                }).done(() => {
+                    this.setState({
+                        refreshing: false
+                    });
+                });
+    }
+
+    loadMore() {
+        if (this.state.refreshing) {
+            return;
+        }
+
+        this.refresh(true);
     }
 
     render() {
         return (
-          <View style={localStyle.container}>
-            <CustomedList listType={this.props.listType} style={localStyle.list}
+            <View style={localStyle.container}>
+                <FlatList style={localStyle.list}
 
-                dataSource={this.props.dataSource}
+                    data={this.state.users}
 
-                renderItem={({item}) => {
-                    return (
-                      <Touchable key={item.id} onPress={() => {}}>
-                        <View style={localStyle.box}>
-                            <UserIcon iconUrl={item.iconUrl}></UserIcon>
-                            <Text style={localStyle.userName}>{item.name}</Text>
-                            <Touchable onPress={() => {}}>
-                                <Ionicons name="md-close" size={20}
-                                    style={localStyle.removeIcon}
-                                    color={Color.inactiveText} />
-                            </Touchable>
-                        </View>
-                      </Touchable>
-                    );
-                }}
-            />
-          </View>
+                    keyExtractor={(item, index) => {
+                        return item.id ? item.id.toString() : index;
+                    }}
+
+                    renderItem={({item}) => {
+                        return (
+                          <Touchable key={item.id} onPress={() => {}}>
+                            <View style={localStyle.box}>
+                                <UserIcon iconUrl={item.iconUrl}></UserIcon>
+                                <Text style={localStyle.userName}>{item.name}</Text>
+                                <Touchable onPress={() => {}}>
+                                    <Ionicons name="md-close" size={20}
+                                        style={localStyle.removeIcon}
+                                        color={Color.inactiveText} />
+                                </Touchable>
+                            </View>
+                          </Touchable>
+                        );
+                    }}
+
+                    refreshing={this.state.refreshing}
+                    onRefresh={this.refresh.bind(this)}
+
+                    onEndReachedThreshold={5}
+                    onEndReached={this.state.hasMore ? this.loadMore.bind(this) : null}
+                />
+            </View>
         );
     }
 }
