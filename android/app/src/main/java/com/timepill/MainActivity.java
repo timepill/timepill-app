4d470cd04637b24f28c6aed5a83a5f0d79c5fb8e
mx1700 <mx1700@gmail.com>
2019-08-25 22:21:23 +0800

    android 增加启动视图

--- android/app/src/main/java/com/timepill/MainActivity.java
+++ android/app/src/main/java/com/timepill/MainActivity.java
@@ -1,8 +1,26 @@
 package com.timepill;
 
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.createSplashLayout());
+    }
+
+    public View createSplashLayout() {
+        LayoutInflater inflate = LayoutInflater.from(this);
+        View view = inflate.inflate(R.layout.splash_layout,null);
+        return view;
+    }
 }
